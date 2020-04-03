57088652a685a78e696e103a03abdac97ae8539c
Timits <will4f21@yahoo.com>
2020-04-03 03:50:18 -0700

    connected to mongodb and test with robomongo

--- rest-api-palylist/index.js
+++ rest-api-palylist/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 //console.log(process.env);
 //const routes = require('./routes/api');
-// app set up
+
+// app express set up
 const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/streamon');
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
 // initialize routes -middleware-
 app.use('/api',require('./routes/api'));
+
+// error handling middleware --if form not completely filled a catch/next response
+app.use(function(err, req, res, next){
+    //console.log(err);
+    res.status(422).send({error: err.message});
+});
 
 // listening on 
 app.listen(process.env.port || 8000, function() {
--- rest-api-palylist/routes/api.js
+++ rest-api-palylist/routes/api.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Stream = require('../models/stream');
 
 // get a list of stream info from db
-router.get('/stream', function(req, res) {
+router.get('/stream', function(req, res, next) {
     res.send({type:'GET'});
 });
 // add to db
-router.post('/stream', function(req, res) {
-    console.log(req.body);
-    res.send({
-    type:'POST',
-    name: req.body.name,
-    title: req.body.title
-    });
+router.post('/stream', function(req, res, next) {
+    //console.log(req.body);
+    let stream = new Stream(req.body);
+    // mongoose save method -- into db in stream.js //stream.save(); use create instead
+    Stream.create(req.body).then(function(){
+        res.send(stream);
+    }).catch(next);
 });
 
 // update to db
-router.put('/stream/:id', function(req, res) {
+router.put('/stream/:id', function(req, res, next) {
     res.send({type:'PUT'});
 });
 
 // delete info in db
-router.delete('/stream/:id', function(req, res) {
+router.delete('/stream/:id', function(req, res, next) {
+    console.log(req.params.id);
     res.send({type:'DELETE'});
 });
 
